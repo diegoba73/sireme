00ce96ecaa723cb891c5a1583edf238792eb330f
“Diego <diegobaulde@gmail.com>
2020-07-03 10:14:17 -0300

    Primer commit

---
+++ resources/views/includes/panel/menu/admin.blade.php
@@ -0,0 +1,25 @@
+<li class="nav-item">
+  <a class="nav-link" href="/sireme/public/home">
+    <i class="ni ni-tv-2 text-danger"></i> Panel Principal
+  </a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="/sireme/public/specialties">
+    <i class="ni ni-planet text-blue"></i> Especialidades
+  </a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="/sireme/public/doctors">
+    <i class="ni ni-single-02 text-red"></i> Médicos
+  </a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="/sireme/public/patients">
+    <i class="ni ni-satisfied text-info"></i> Pacientes
+  </a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="/sireme/public/appointments">
+    <i class="ni ni-time-alarm text-primary"></i> Citas médicas
+  </a>
+</li>
